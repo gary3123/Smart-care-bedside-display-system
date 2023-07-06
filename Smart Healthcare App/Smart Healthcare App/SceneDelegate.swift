c9abcdac1de7c8e8df76633fa852c7d921dc2555
Gary Lin <ban103123@gmail.com>
2023-07-06 13:57:10 +0800

    修正 SceneDelegate

--- Smart Healthcare App/Smart Healthcare App/SceneDelegate.swift
+++ Smart Healthcare App/Smart Healthcare App/SceneDelegate.swift
@@ -13,24 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-            
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-                    
-                    // MARK: Without NavigationController
-                    // let rootVC = MainVC(nibName: String(describing: MainVC.self), bundle: nil)
-                    // MARK: With NavigationController
-                     let rootVC = UINavigationController(rootViewController: MainViewController())
-                    
-                    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-                    window?.windowScene = windowScene
-                    window?.rootViewController = rootVC //navController
-                    window?.makeKeyAndVisible()
-            
-            // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-            // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-            // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let rootVC = Login(nibName: "Login", bundle: nil)
+        let navigationController = UINavigationController(rootViewController: rootVC)
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
